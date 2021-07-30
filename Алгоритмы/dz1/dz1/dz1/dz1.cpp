6e690908c3b46f26a5f408c4f6289094b6e8747e
Seer77 <seer5@mail.ru>
2021-07-30 19:28:49 +0300

    вdz1

---
+++ Алгоритмы/dz1/dz1/dz1/dz1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    int a, b;
+    std::cout << "Input number one:" << std::endl;
+    std::cin >> a;
+    std::cout << "Input number two:" << std::endl;
+    std::cin >> b;
+    int c = a * b;
+    std::cout << "Product of a and b = "; std::cout << c;
+}
+
