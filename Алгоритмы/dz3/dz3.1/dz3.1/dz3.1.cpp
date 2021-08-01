addbb09ed4bf186ff8d574d3a6daf3a596228ea1
Seer77 <seer5@mail.ru>
2021-08-01 18:25:15 +0300

    dz3

---
+++ Алгоритмы/dz3/dz3.1/dz3.1/dz3.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int main()
+{
+    int number;
+    std::cout << "Enter an integer, positive number:";
+    std::cin >> number;
+    int d{ 0 };
+    int i{ 1 };
+    while (i <= number)
+    {
+        if (number % i++ == 0)
+        {
+            d++;
+        }
+        else
+        {
+            continue;
+        }
+        if (d == 3)
+        {
+            break;
+        }
+    }
+    d == 3 ? std::cout << "Number is not prime" << std::endl : std::cout << "The number is prime" << std::endl;
+
+    size_t SUMM { 0 };
+    for (size_t i = 1; i <= 10; i++)
+    {
+        SUMM = SUMM + i;
+    }
+    std::cout << SUMM;
+}
